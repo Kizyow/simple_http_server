cdde53bc2329c45a6b384faddbc38a4ae7b60494
BuildTools <unconfigured@null.spigotmc.org>
2022-06-09 14:26:57 +0200

    http

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -19,14 +19,18 @@ public class HttpServer {
 
         try {
 
+            // Lancement du serveur Web
             ServerSocket serverSocket = new ServerSocket(port);
             System.out.println("[WebServer] Listening on port " + port);
 
+            // Le serveur tourne à l'infini
             while (true) {
 
+                // En attente d'une connexion
                 Socket socket = serverSocket.accept();
                 System.out.println("[WebServer] Fetching connection (" + socket.getRemoteSocketAddress() + ")");
 
+                // Lecture de la requete
                 InputStreamReader isr = new InputStreamReader(socket.getInputStream());
                 BufferedReader br = new BufferedReader(isr);
 
@@ -37,6 +41,7 @@ public class HttpServer {
 
                 try {
 
+                    // On répond à la requete
                     PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
                     printWriter.println("HTTP/1.1 200 OK \n");
                     printWriter.flush();
@@ -44,12 +49,15 @@ public class HttpServer {
                     boolean accept = AddressChecker.match(socket.getInetAddress(), xmlData.getAccept());
                     boolean deny = AddressChecker.match(socket.getInetAddress(), xmlData.getReject());
 
+                    // on vérifie l'adresse IP
                     if (!accept || deny) {
                         printWriter.println("<html><body><h1>Connection refused</h1><p>Your IP address is rejected</p></body></html>");
                         printWriter.flush();
 
+                    // on vérifie si c'est la racine
                     } else if (url.equalsIgnoreCase("/")) {
 
+                        // s'il y a un index, le mettre par défaut
                         if (xmlData.hasIndex()) {
 
                             url = "/index.html";
@@ -60,6 +68,7 @@ public class HttpServer {
                             socket.getOutputStream().write(file);
                             fis.close();
 
+                        // sinon, on génère nous-même l'index de la racine
                         } else {
 
                             String htmlListFiles = "";
@@ -73,21 +82,47 @@ public class HttpServer {
                                 }
                             }
 
-                            printWriter.println("<html><body><h1>Index of</h1>" + htmlListFiles + "</body></html>");
+                            printWriter.println("<html><body><h1>Index of " + url + "</h1>" + htmlListFiles + "</body></html>");
                             printWriter.flush();
 
                         }
 
+                    // si c'est une requete vers un fichier en particulier
                     } else {
 
-                        FileInputStream fis = new FileInputStream(xmlData.getRoot() + url);
-                        byte[] file = fis.readAllBytes();
+                        File file = new File(xmlData.getRoot() + url);
 
-                        socket.getOutputStream().write(file);
-                        fis.close();
+                        // on vérifie si c'est un dossier
+                        if (file.isDirectory()) {
+
+                            // faire l'index du dossier
+                            String htmlListFiles = "";
+                            File[] listFiles = file.listFiles();
+
+                            if (listFiles != null) {
+                                htmlListFiles += "<ul>";
+                                for (File f : listFiles) {
+                                    htmlListFiles += "<li><a href=" + file.getName() + "/" + f.getName() + ">" + f.getName() + "</li>";
+                                }
+                            }
+
+                            printWriter.println("<html><body><h1>Index of " + url + "</h1>" + htmlListFiles + "</body></html>");
+                            printWriter.flush();
+
+                        // sinon on envoie le fichier au client
+                        } else {
+
+                            FileInputStream fis = new FileInputStream(file);
+                            byte[] fileArray = fis.readAllBytes();
+
+                            socket.getOutputStream().write(fileArray);
+                            fis.close();
+
+                        }
 
                     }
 
+                    // on répond et on ferme la connexion
                     System.out.println("[WebServer] Replied on the request");
                     socket.close();
 
